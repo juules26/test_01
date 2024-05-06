7886cd6cbf7bfe1c5666c9bd1245b8acd6a53fdc
juules26 <juules26@gmail.com>
2024-05-06 19:45:37 +0200

    button colors added (w/interaction)

--- sudoku_game.py
+++ sudoku_game.py
@@ -19,6 +19,38 @@ def create_design(window):
     label = tk.Label(label_frame, text="SUDOKU", font=('Helvetica',15))
     label.pack(pady=20)
 
+# Create Buttons for difficulty
+    button_easy = tk.Button(
+        label_frame,
+        text="Dumb",
+        background= "Green yellow",
+        foreground="Black",
+        activebackground="Green4",
+        activeforeground="Black",
+        width=10
+        )
+    button_easy.pack(pady=10,padx=10,side=tk.LEFT)
+    button_medium = tk.Button(
+        label_frame,
+        text="Meh, okay",
+        background= "gold2",
+        foreground="Black",
+        activebackground="chocolate3",
+        activeforeground="Black",
+        width=10
+        )
+    button_medium.pack(pady=10, padx=10, side=tk.LEFT)
+    button_hard = tk.Button(
+        label_frame,
+        text="Good luck",
+        background= "red2",
+        foreground="Black",
+        activebackground="black",
+        activeforeground="red2",
+        width=10
+        )
+    button_hard.pack(pady=10, padx=10, side=tk.LEFT)
+    
 # create a subframe for the grid frames
     grid_frame = ttk.Frame(main_frame)
     grid_frame.pack()
@@ -26,14 +58,14 @@ def create_design(window):
 # create gird frames inside the grid frame(master)   
     for i in range(3):
         for j in range(3):
-            frame = ttk.Frame(grid_frame, width=100, height=100, relief=tk.RIDGE, borderwidth=3)
+            frame = ttk.Frame(grid_frame, width=100, height=100, relief=tk.GROOVE, borderwidth=3)
             frame.grid(row=i, column=j, padx=1,pady=1)
 
             for x in range(3):
                 for y in range(3):
                     background_color = "LightSkyBlue1"
                     num_entry = tk.Entry(frame,width=3,background=background_color,font=("Helvetica",12),justify="center",highlightthickness=1)
-                    num_entry.grid(row=x, column=y, padx=2,pady=2)
+                    num_entry.grid(row=x, column=y, padx=1,pady=1)
                     vlcm = (window.register(validate_number), "%P")
                     num_entry.configure(validate="key",validatecommand=vlcm)
                     num_entry.config(insertbackground=background_color)
